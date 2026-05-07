2e6ed3b1d71694fd2d28af896f20ca51aaa71a32
hafsalahouari20-cloud <hafsa.lahouari20@gmail.com>
2026-05-07 03:42:12 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -208,6 +208,7 @@ function cdnPrefixImages(): Plugin {
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
   return {
+    base: '/association-al-kendi/',
     server: {
       host: "::",
       port: 8080,
